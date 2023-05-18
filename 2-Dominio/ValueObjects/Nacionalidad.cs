9bdc8600b1cb097ba39c0f73ec992054480cb6d1
MauroMauro <blur.benitez@gmail.com>
2023-05-18 19:19:12 -0300

    Agregados Entidad y Value Objects

---
+++ 2-Dominio/ValueObjects/Nacionalidad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Dominio.ValueObjects
+{
+    internal class Nacionalidad
+    {
+        private String valor;
+
+        public Nacionalidad(String valor)
+        {
+            this.DebeContenerMasDe3Caracteres(valor);
+            this.valor = valor;
+        }
+
+        public String Valor()
+        {
+            return this.valor;
+        }
+
+        private void DebeContenerMasDe3Caracteres(String valor)
+        {
+            if(valor.Length < 3)
+            {
+                throw new Exception("La nacionalidad debe contener mas de tres caracteres");
+            }
+        }
+    }
+}
