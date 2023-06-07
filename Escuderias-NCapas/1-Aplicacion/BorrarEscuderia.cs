40aa08e377e7710c0def7a4a5d2ab8c1b0dd1c79
Nicolas Jimenez <Nicolasdanieljimenez@hotmail.com>
2023-06-07 20:37:31 -0300

    Añadido caso de uso

---
+++ Escuderias-NCapas/1-Aplicacion/BorrarEscuderia.cs
@@ -0,0 +1,21 @@
+﻿using _1_Aplicacion.DTO;
+using _2_Dominio;
+using _2_Dominio.Repositorio;
+
+namespace _1_Aplicacion
+{
+    public class BorrarEscuderia
+    {
+        private IEscuderiaRepositorio repositorio;
+
+        public BorrarEscuderia(IEscuderiaRepositorio repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        public void ejecutar(EscuderiaDTO escuderia)
+        {
+            this.repositorio.borrarEscuderia(new Escuderia(escuderia.Id(), escuderia.Nombre(), escuderia.Nacionalidad(), escuderia.AnioFundacion(), escuderia.Motores()));
+        }
+    }
+}
