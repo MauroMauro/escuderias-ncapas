bf54d20c64dd21a836a90189d708403f8d63fdb9
Nicolas Jimenez <Nicolasdanieljimenez@hotmail.com>
2023-06-10 11:13:38 -0300

    Creado repo

---
+++ Escuderias-NCapas/3-Infraestructura/EscuderiaRepositorioMongoDB.cs
@@ -0,0 +1,30 @@
+﻿using _2_Dominio;
+using _2_Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace _3_Infraestructura
+{
+    public class EscuderiaRepositorioMongoDB : IEscuderiaRepositorio
+    {
+        public void borrarEscuderia(Escuderia escuderia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void grabar(Escuderia escuderia)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public List<Escuderia> obtenerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
