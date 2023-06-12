fa56febf566746b508f7217ea766d1b053603228
JessSena <jessicaalesena@gmail.com>
2023-06-11 23:13:48 -0300

    añadir ActualizarEscuderia a la capa de aplicación

---
+++ Escuderias-NCapas/1-Aplicacion/ActualizarEscuderia.cs
@@ -0,0 +1,26 @@
+﻿using _1_Aplicacion.DTO;
+using _2_Dominio.Repositorio;
+using _2_Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Aplicacion
+{
+    public class ActualizarEscuderia
+    {
+        private IEscuderiaRepositorio repositorio;
+
+        public ActualizarEscuderia(IEscuderiaRepositorio repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        public void ejecutar(EscuderiaDTO escuderia)
+        {
+            this.repositorio.actualizarEscuderia(new Escuderia(escuderia.Id(), escuderia.Nombre(), escuderia.Nacionalidad(), escuderia.AnioFundacion(), escuderia.Motores()));
+        }
+    }
+}
